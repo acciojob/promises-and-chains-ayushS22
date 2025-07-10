d8125e6cb3467999ce9be4a6aa4626e306eba2f5
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-07-10 11:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-//your JS code here. If required.
- const name = document.getElementById("name").value.trim();
- const age = document.getElementById("age").value.trim();
- /* validation lagao  */
-if(name === " " || age === " "){
-	 alert(Please enter valid details.);
+const name = document.getElementById("name").value.trim();
+const age = document.getElementById("age").value.trim();
+
+/* Validation */
+if (name === "" || age === "") {
+  alert("Please enter valid details.");
+  return;
 }
- /* Convert age to number for comparison */
-      const ageNumber = Number(age);
 
-// promise create
+/* Convert age to number */
+const ageNumber = Number(age);
 
-const myAgePromise = new promise( (resole,reject) =>{
-	setTimeOut(()=>{
-		if( myAgePromise > 18){
-			resolve(name);
-		}
-		else{
-			reject(name);
-		}
-	},4000);
+/* Create Promise */
+const myAgePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (ageNumber > 18) {
+      resolve(name);
+    } else {
+      reject(name);
+    }
+  }, 4000);
 });
 
-// handle promise results
-myAgePromise.then((name)=>{
-	alert('Welcome, . You can vote.');
-}).catch(()=>{
-	alert("Oh sorry . You aren't old enough.");
-})
-
+/* Handle Promise */
+myAgePromise
+  .then((userName) => {
+    alert(`Welcome, ${userName}. You can vote.`);
+  })
+  .catch((userName) => {
+    alert(`Oh sorry ${userName}. You aren't old enough.`);
+  });
