e20dab1b93738d0092a61cd1a3eebd714d2d03fc
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-07-10 10:49:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+ const name = document.getElementById("name").value.trim();
+ const age = document.getElementById("age").value.trim();
+ /* validation lagao  */
+if(name === " " || age === " "){
+	 alert(Please enter valid details.);
+}
+ /* Convert age to number for comparison */
+      const ageNumber = Number(age);
+
+// promise create
+
+const myAgePromise = new promise( (resole,reject) =>{
+	setTimeOut(()=>{
+		if( myAgePromise > 18){
+			resolve(name);
+		}
+		else{
+			reject(name);
+		}
+	},4000);
+});
+
+// handle promise results
+myAgePromise.then((name)=>{
+	alert(Welcome, . You can vote.);
+}).catch(()=>{
+	alert(Oh sorry . You aren't old enough.);
+})
+
